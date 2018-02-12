579e68e68043423166c7ebaa6b2027ffd7d70ab6
Paula Nowicka <paula.nowicka@yahoo.co.uk>
2018-02-12 15:41:53 +0000

    Fixed animation bugs. Completed tutorial level 1 structure

--- src/js/app.js
+++ src/js/app.js
@@ -1,14 +1,23 @@
 $(() => {
   console.log('hello');
-  // const $doggy = $('.dog');
-  //
-  //
-  //
+  const $doggy = $('.dog');
+  const $chest = $('.closedChest');
+  const $key = $('.key');
+  let key = false;
+  const $wall = $('.wall');
+  const $heading = $('#heading');
+
+
+  //original animation run.
+
+
   // function move(e) {
   //   const key = e.keyCode;
   //   $('body').off('keypress');
-  //   if (key === 68) $doggy.html('<img src="/images/dog_right.gif" alt="Doggy">');
-  //   $('body').off('keypress');
+  //   console.log('hello');
+  //   if (key === 68) $doggy.html('<img src="/images/dog_right.gif" alt="Doggy">') && $doggy.
+  //
+  //     $('body').off('keypress');
   //   if (key === 65) $doggy.html('<img src="/images/dog_left.gif" alt="Doggy">');
   // }
   // function stop(e) {
@@ -18,7 +27,54 @@ $(() => {
   //   $('body').on('keypress');
   //   if (key === 65) $doggy.html('<img src="/images/dog_left.png" alt="Doggy">');
   // }
-  //
+
   // $('body').keydown(move);
   // $('body').keyup(stop);
+  $chest.click(function(){
+    $chest.attr('src', '/images/chest_open.png');
+    $key.show();
+  });
+
+
+
+  $(document)
+    .keydown(function(e) {
+      switch (e.which) {
+        case 65:
+          if ($doggy.attr('src').match(/png/)){
+            $doggy.attr('src',
+              '/images/dog_left.gif');
+          }
+          $doggy.css({left: '-=10' });
+          break;
+        case 68:
+          if (!key) {
+            if ($doggy.position().left >= 850) {
+              return;
+            }if ($doggy.position().left === 300){
+              $heading.text('Use A and D to move');
+              console.log('working');
+            }
+          }
+          if($doggy.attr('src').match(/png/)) {
+            $doggy.attr('src', '/images/dog_right.gif');
+          }
+          $doggy.css({ left: '+=10' });
+          break;
+      }
+    })
+
+    .keyup(function() {
+      const src = $doggy.attr('src').replace('.gif', '.png');
+      $doggy.attr('src', src);
+    });
+
+  $key.click(function(){
+    $key.remove();
+    console.log('please');
+    $wall.remove();
+    console.log('help');
+    key = true;
+  });
+
 });
